40166561e0698b2fef366de3e538e3e07c65a1c2
Alexandr Köln <koelnfrei@gmail.com>
2018-02-21 19:16:55 +0300

    Add feature file and steps scaffolds

---
+++ SeleniumTestTask/Features/YandexSearch.feature
@@ -0,0 +1,10 @@
+﻿Feature: Search Selenium in Yandex
+
+@yandex
+Scenario: Search Sleenium and open Wiki page
+	Given I open yandex main page
+	When I enter in search area "Selenium"
+	And I press search button
+	Then the result should be 120 on the screen
+	When I click first link with "Wikipedia" in title
+	Then Open wiki page about "Selenium"
---
+++ SeleniumTestTask/Steps/YandexSearchSteps.cs
@@ -0,0 +1,44 @@
+﻿using TechTalk.SpecFlow;
+
+namespace SeleniumTestTask.Steps
+{
+	[Binding, Scope(Tag = "yandex")]
+	public class YandexSearchSteps
+	{
+		[Given(@"I open yandex main page")]
+		public void GivenIOpenYandexMainPage()
+		{
+			ScenarioContext.Current.Pending();
+		}
+
+		[When(@"I enter in search area ""(.*)""")]
+		public void WhenIEnterInSearchArea(string selenium0)
+		{
+			ScenarioContext.Current.Pending();
+		}
+
+		[When(@"I press search button")]
+		public void WhenIPressSearchButton()
+		{
+			ScenarioContext.Current.Pending();
+		}
+
+		[When(@"I click first link with ""(.*)"" in title")]
+		public void WhenIClickFirstLinkWithInTitle(string wikipedia0)
+		{
+			ScenarioContext.Current.Pending();
+		}
+
+		[Then(@"the result should be (.*) on the screen")]
+		public void ThenTheResultShouldBeOnTheScreen(int p0)
+		{
+			ScenarioContext.Current.Pending();
+		}
+
+		[Then(@"Open wiki page about ""(.*)""")]
+		public void ThenOpenWikiPageAbout(string selenium0)
+		{
+			ScenarioContext.Current.Pending();
+		}
+	}
+}
